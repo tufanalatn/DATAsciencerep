85ea29ca4206ed7d2a4d0432405e46258cd03a3b
Tufan ALATAN <tufan.alatan@merkez.uls>
2019-03-29 11:16:45 +0300

    This is first assigment with Gitgub

---
+++ First_assigment.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This is for CapsTone Project 
+import pandas as pd
+import numpy as ny 
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
